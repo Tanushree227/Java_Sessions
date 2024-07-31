07f883dbfe7e348c67ec31ff2b013f4414d0c0b8
Tanushree227 <tanushreedas813@gmail.com>
2024-07-31 17:27:14 +0530

    java datatypes

--- Program1.java
+++ Program1.java
@@ -2,5 +2,12 @@ public class Program1
 {
     public static void main(String[] args) {
         System.out.println("Hakuna Matata");
+        System.out.println("Data Types");
+        System.out.println("1. Integer Values: 1");
+        System.out.println("2. Float Values: 1.123");
+        System.out.println("3. Double Values: 1.904042897");
+        System.out.println("4. Boolean Values: true and false");
+        System.out.println("5. Char Values: a");
+
     }
 }
